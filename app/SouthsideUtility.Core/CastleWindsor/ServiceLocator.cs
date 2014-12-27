b60d7223046a6205b02f511900a4fd416a47edcd
Tom Cabanski <tom@cabanski.com>
2014-12-27 12:01:11 -0600

    Roughing in

---
+++ app/SouthsideUtility.Core/CastleWindsor/ServiceLocator.cs
@@ -0,0 +1,13 @@
+﻿using Castle.Windsor;
+
+namespace SouthsideUtility.Core.CastleWindsor
+{
+    public static class ServiceLocator
+    {
+        static ServiceLocator()
+        {
+            Container = new WindsorContainer();
+        }
+        public static IWindsorContainer Container { get; private set; }
+    }
+}
